48c4bba448d4e52fb2d2ba8f4f3c6557f06e0d21
Walid Saifi <saifiwalid21@gmail.com>
2024-03-06 11:33:53 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthentificationController;
+use App\Http\Controllers\ForgetpasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login',[AuthentificationController::class,'pagelogin'])->name('login');
+
+Route::post('/login',[AuthentificationController::class,'loginAction'])->name('loginAction');
+Route::get('/register',[AuthentificationController::class, 'pageregister'])->name('pageregister');
+
+Route::post('/register.save',[AuthentificationController::class, 'registerSave'])->name('register.save');
+Route::post('/', [AuthentificationController::class, 'destroy'])->name('logout');
+Route::get('/forget_password', [ForgetpasswordController::class, 'fogetpassword'])->name('foget.password');
+Route::post('/forget_password', [ForgetpasswordController::class, 'fogetpasswordPost'])->name('foget.passwordPost');
+Route::get('/rest_password/{token}', [ForgetpasswordController::class, 'rest_password'])->name('rest.password');
+Route::post('/rest_password', [ForgetpasswordController::class, 'rest_passwordPost'])->name('rest.passwordPost');
