2599869ff31d315a23249420a252f0547579e0b9
Walid Saifi <saifiwalid21@gmail.com>
2024-03-07 16:49:09 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Admin.dashbord');
 });
+Route::get('/ajouter', function () {
+    return view('Admin.Ajouterevent');
+});
 Route::get('/login',[AuthentificationController::class,'pagelogin'])->name('login');
 
 Route::post('/login',[AuthentificationController::class,'loginAction'])->name('loginAction');
