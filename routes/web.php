34bab0fbbf67a00bbd3cf883eb202d175759277a
Walid Saifi <saifiwalid21@gmail.com>
2024-03-07 16:24:00 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Admin.dashbord');
 });
 Route::get('/login',[AuthentificationController::class,'pagelogin'])->name('login');
 
