6ebcfcd258fa314cee2ab8d9a7f9a0850993c187
Walid Saifi <saifiwalid21@gmail.com>
2024-03-10 08:46:05 +0000

    Update OrganisateurController.php

--- app/Http/Controllers/OrganisateurController.php
+++ app/Http/Controllers/OrganisateurController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class OrganisateurController extends Controller
 {
-    public  
+    public function  index(){
+        return view('Organisateur.dashbord');
+    }
+
 }
