d75b3faff05bd1656def2cbbed52bced61c5172f
Walid Saifi <saifiwalid21@gmail.com>
2024-03-10 10:24:09 +0000

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function  ajouter(){
+        return view('Admin.ajouterCat');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()->route('categories.index')->with('success', 'Category created successfully.');
+    }
 }
