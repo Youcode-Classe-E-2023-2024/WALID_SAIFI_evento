eea3bf034fc1bfc8e190b99c8250e921eaf87c61
Walid Saifi <saifiwalid21@gmail.com>
2024-03-10 12:33:31 +0000

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public  function  index(){
+             $categories = Category::all();
+           return view('Admin.Category' , compact('categories'));
+    }
     public function  ajouter(){
         return view('Admin.ajouterCat');
     }
@@ -16,8 +20,43 @@ class CategoryController extends Controller
             'name' => 'required|string|max:255',
         ]);
 
-        Category::create($request->all());
+        Category::create([
+            'name' => $request->name,
+        ]);
 
-        return redirect()->route('categories.index')->with('success', 'Category created successfully.');
+        return redirect()->route('ajouter.cat')->with('success', 'Catégorie créée avec succès.');
     }
+
+    public function edit($id)
+    {
+        $category = Category::findOrFail($id);
+        return view('Admin.editCat', compact('category'));
+    }
+
+    // Méthode pour supprimer une catégorie
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return redirect()->route('afficheCat')->with('success', 'Catégorie supprimée avec succès.');
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $category = Category::findOrFail($id);
+        $category->update([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('afficheCat')->with('successUpdate', 'Catégorie mise à jour avec succès.');
+    }
+
+
+
 }
