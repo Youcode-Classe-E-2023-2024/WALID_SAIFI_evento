6936e88b2c54db6210895d3724c2c04cb5cd07f9
Walid Saifi <saifiwalid21@gmail.com>
2024-03-10 16:09:56 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,12 @@ Route::get('/', function () {
 Route::get('/ajouter', function () {
     return view('Admin.Ajouterevent');
 });
-Route::get('/login',[AuthentificationController::class,'pagelogin'])->name('login');
 
-Route::post('/login',[AuthentificationController::class,'loginAction'])->name('loginAction');
+    Route::get('/login', [AuthentificationController::class, 'pagelogin'])->name('login');
+    Route::post('/login', [AuthentificationController::class, 'loginAction'])->name('loginAction');
+
+
 Route::get('/register',[AuthentificationController::class, 'pageregister'])->name('pageregister');
-
 Route::post('/register.save',[AuthentificationController::class, 'registerSave'])->name('register.save');
 Route::post('/', [AuthentificationController::class, 'destroy'])->name('logout');
 Route::get('/forget_password', [ForgetpasswordController::class, 'fogetpassword'])->name('foget.password');
@@ -36,19 +37,22 @@ Route::post('/forget_password', [ForgetpasswordController::class, 'fogetpassword
 Route::get('/rest_password/{token}', [ForgetpasswordController::class, 'rest_password'])->name('rest.password');
 Route::post('/rest_password', [ForgetpasswordController::class, 'rest_passwordPost'])->name('rest.passwordPost');
 /*-------------------------------------------------Admin*-------------------------------------------------------------*/
-Route::get('/admin/dashbord',[adminrController::class, 'index'])->name('admin.dashbord');
-Route::post('/deconneter',[AuthentificationController::class, 'destroy'])->name('logout');
-Route::get('/categoriser/index',[CategoryController::class, 'index'])->name('afficheCat');
-Route::get('/ajoutercatgorier',[CategoryController::class, 'ajouter'])->name('ajouter.cat');
-Route::post('/catgorierajouter',[CategoryController::class, 'store'])->name('Category.stort');
-Route::delete('/deletCategory/{id}', [CategoryController::class, 'destroy'])->name('delete.cat');
-Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('edit.cat');
-Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('update.cat');
-
-
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashbord', [adminrController::class, 'index'])->name('admin.dashbord');
+    Route::post('/deconnecter', [AuthentificationController::class, 'destroy'])->name('logout');
+    Route::get('/categoriser/index', [CategoryController::class, 'index'])->name('afficheCat');
+    Route::get('/ajoutercatgorier', [CategoryController::class, 'ajouter'])->name('ajouter.cat');
+    Route::post('/catgorierajouter', [CategoryController::class, 'store'])->name('Category.stort');
+    Route::delete('/deletCategory/{id}', [CategoryController::class, 'destroy'])->name('delete.cat');
+    Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('edit.cat');
+    Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('update.cat');
+});
 
 
 
 /*----------------------------                 Oraganisateur                                        ----------------*/
-Route::get('/organisateur/dashbord',[OrganisateurController::class, 'index'])->name('org.dashbord');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/organisateur/dashbord',[OrganisateurController::class, 'index'])->name('org.dashbord');
+});
+
 
