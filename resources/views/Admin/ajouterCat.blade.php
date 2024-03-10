33c61d3181f7ddfe2663579e8e7829b4697a05f4
Walid Saifi <saifiwalid21@gmail.com>
2024-03-10 10:16:38 +0000

    Create ajouterCat.blade.php

---
+++ resources/views/Admin/ajouterCat.blade.php
@@ -0,0 +1,32 @@
+@extends('Admin.layout');
+
+@section('content')
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Ajouter un catégoriser</h1>
+                </div>
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="card bg-dark text-white">
+                            <div class="card-body">
+                                <form action="" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="titre" class="form-label">Nom de catégoriser</label>
+                                        <input type="text" class="form-control"  name="categoriser">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Ajouter catégoriser</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+
+@endsection
